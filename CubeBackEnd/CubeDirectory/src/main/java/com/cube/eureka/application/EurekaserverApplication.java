cdd2c4bcb00e03e2f7df2152a17e64853b870ede
vipinpjohn <vipin.p.john@gmail.com>
2020-06-15 23:47:39 +0530

    Eureka changes

--- CubeBackEnd/CubeDirectory/src/main/java/com/cube/eureka/application/EurekaserverApplication.java
+++ CubeBackEnd/CubeDirectory/src/main/java/com/cube/eureka/application/EurekaserverApplication.java
@@ -1,12 +1,8 @@
 package com.cube.eureka.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,9 +11,4 @@ public class EurekaserverApplication {
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
-	@ConditionalOnMissingBean
-	@Bean
-	public HttpTraceRepository httpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
-	}
 }
